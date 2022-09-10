4bbfbf678bbc30b44613ca7c4740aaf48bf363fe
Ha Nguyen <vietha.nguyen3110@gmail.com>
2022-09-09 22:25:17 -0700

    first commit

---
+++ sametree.js
@@ -0,0 +1,58 @@
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const p = [1, 2, 3], q = [1, 2, 3]
+// console.log(isSameTree(p, q))  //expect true;
+
+// const p = [1, 2], q = [1, null, 2]
+// console.log(isSameTree(p.q)) //expect false;
---
+++ twoSum-BTS.js
@@ -0,0 +1,80 @@
+// Given the root of a Binary Search Tree and a target number k, return true if there exist two elements in the BST such that their sum is equal to the given target. 
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function (root, k) {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const root = [5, 3, 6, 2, 4, null, 7], k = 28;
+// console.log(findTarget(root, k)) //expect false
+
+// const root = [5, 3, 6, 2, 4, null, 7], k = 9
+// console.log(findTarget(root, k)) //expect true
