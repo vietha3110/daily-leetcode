2b8c34bc4d0f0679db10ba6cf8a7ab1699eb57c2
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-02-23 10:41:06 -0800

    twosum-brutforce solution js

---
+++ 1-TwoSum.js
@@ -0,0 +1,11 @@
+function twoSum(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+}
+
+console.log(twoSum(arr=[3,2,4], target = 6))
