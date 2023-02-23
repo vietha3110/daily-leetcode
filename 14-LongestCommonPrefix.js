0fced46fe0a823d7eb1e62ed9f49b3055a676a30
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-02-23 15:45:29 -0800

    26-removeDuplicate js

---
+++ 14-LongestCommonPrefix.js
@@ -0,0 +1,4 @@
+function longgestCommonPrefix(str) {
+    let obj = {}
+
+}
---
+++ 26-removeDup.js
@@ -0,0 +1,11 @@
+var removeDuplicates = function(nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums.splice(i + 1, 1);
+            i--;
+        }
+    }
+    return nums;
+};
+
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
