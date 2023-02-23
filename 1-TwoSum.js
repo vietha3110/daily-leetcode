d1fe57b95d52768241ac35f2e73343c04f9dccc9
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-02-23 10:57:03 -0800

    twosum-brutforce solution js hash

--- 1-TwoSum.js
+++ 1-TwoSum.js
@@ -1,10 +1,23 @@
+// function twoSum(arr, target) {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i + 1; j < arr.length; j++) {
+//             if (arr[i] + arr[j] === target) {
+//                 return [i, j]
+//             }
+//         }
+//     }
+// }
+
 function twoSum(arr, target) {
+    let obj = {}; 
+    let result = [];
     for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] + arr[j] === target) {
-                return [i, j]
-            }
+        if (arr[i] in obj) {
+            let j = obj[arr[i]];
+            result.push(j, i);
+            return result;
         }
+        obj[target - arr[i]] = i
     }
 }
 
