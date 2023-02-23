dc9c1a3cf33e47351498b180ae6152931d31cdad
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-02-23 11:04:01 -0800

    twosum-brutforce solution py hash

--- 1-TwoSum.py
+++ 1-TwoSum.py
@@ -1,11 +1,24 @@
-def twoSum(list, int): 
+# def twoSum(list, int): 
+#     i = 0
+#     while i < len(list): 
+#         j = i + 1 
+#         while j < len(list): 
+#             if ((list[i] + list[j]) == int):
+#                 return [i, j]
+#             j += 1
+#         i += 1
+
+def twoSum(nums, target):
+    dict_num = {}
+    list_result = []
     i = 0
-    while i < len(list): 
-        j = i + 1 
-        while j < len(list): 
-            if ((list[i] + list[j]) == int):
-                return [i, j]
-            j += 1
+    while i < len(nums):
+        if (nums[i] in dict_num): 
+            j = dict_num[nums[i]]
+            list_result.append(i)
+            list_result.append(j)
+            return list_result
+        dict_num[target - nums[i]] = i
         i += 1
 
 
