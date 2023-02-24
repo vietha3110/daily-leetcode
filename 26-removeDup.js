f5337e87667b563d25667d5afc27aa7fd29d0749
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-02-24 14:45:05 -0800

    26-removeDup python.

--- 26-removeDup.js
+++ 26-removeDup.js
@@ -1,11 +1,33 @@
-var removeDuplicates = function(nums) {
-    for (let i = 0; i < nums.length - 1; i++) {
-        if (nums[i] === nums[i + 1]) {
-            nums.splice(i + 1, 1);
-            i--;
+// var removeDuplicates = function(nums) {
+//     for (let i = 0; i < nums.length - 1; i++) {
+//         if (nums[i] === nums[i + 1]) {
+//             nums.splice(i + 1, 1);
+//             i--;
+//         }
+//     }
+//     return nums;
+// };
+var removeDuplicates = function (nums) {
+    let pointer = 0;
+    for (let i = 0; i < nums.length; i++) {
+        console.log(nums)
+        if (i + 1 === nums.length  || nums[i] !== nums[i + 1]) {
+            nums[pointer] = nums[i];
+            console.log(nums)
+            pointer++;
         }
     }
-    return nums;
-};
+    return pointer
+}
+
+
+
+
+
+
+
+
+
+
 
 console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
--- 26-removeDup.py
+++ 26-removeDup.py
@@ -1,11 +1,11 @@
 def removeDuplicates(arr): 
+    pointer = 0 
     i = 0 
-    print(3, arr)
-    while i < len(arr): 
-        if arr[i] == arr[i + 1]: 
-            arr = arr[i + 1 : len(arr) - 1]
-            print(arr)
+    while i < len(arr):
+        if (i + 1 == len(arr) or arr[i] != arr[i + 1]):
+            arr[pointer] = arr[i]
+            pointer += 1
         i += 1
-    return len(arr)
+    return pointer
 
 print(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
