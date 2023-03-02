e3693b5a7213bcd9246a56d1340aafea2dea32c2
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-03-01 22:17:28 -0800

    191-NumberOf1Bit js

---
+++ 191-NumberOf1Bits.js
@@ -0,0 +1,12 @@
+function hammingWeight(n) {
+    let count = 0; 
+    while (n > 0) {
+        if (n % 2 === 1) {
+            count++
+        }
+        n = Math.floor(n / 2);
+    }
+    return count;
+}
+
+console.log(hammingWeight(11))
