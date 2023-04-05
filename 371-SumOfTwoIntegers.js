c3f283b7a38a43bb7fcaeea5b14b7d4e4cbdd201
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-04-05 09:55:40 -0700

    371-SumOfTwoIntegers js

--- 371-SumOfTwoIntegers.js
+++ 371-SumOfTwoIntegers.js
@@ -2,3 +2,21 @@
 
 // Input: a = 1, b = 2
 // Output: 3
+function sum(a, b) {
+    while (b !== 0) {
+        const carry = a & b;         
+        a = a ^ b;
+        b = carry << 1;
+    }
+    return a;
+}
+
+console.log(sum(10, 1))
+//recursive 
+function add(a, b) {
+    if (b === 0) {
+        return a;
+    } else {
+        return add(a ^ b, (a && b) << 1);
+    }
+}
