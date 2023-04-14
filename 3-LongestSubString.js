5003f83c36a71fc4ba3dd7dd7669a97da6b3d9ec
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-04-14 10:16:35 -0700

    409-Longest Palindrome js

--- 3-LongestSubString.js
+++ 3-LongestSubString.js
@@ -1,24 +1,32 @@
+// function lengthOfLongestSubstring(s) {
+//     let max = 0; 
+    
+//     for (let i = 0; i < s.length; i++) {
+//         let check = new Set();
+//         check.add(s[i]);
+//         let count = 1;
+//         for (let j = i + 1; j < s.length; j++){
+//             if (check.has(s[j])) {
+//                 if (count > max) max = count;
+//                 break;
+//             } else {
+//                 check.add(s[j]); 
+//                 count++;
+//             }
+//         }
+//         if (count > max) max = count; 
+//     }
+   
+//     return max;
+
+// }
+
+// console.log(lengthOfLongestSubstring("abcabcbb"))
+
 function lengthOfLongestSubstring(s) {
     let max = 0; 
-    
-    for (let i = 0; i < s.length; i++) {
-        let check = new Set();
-        check.add(s[i]);
-        let count = 1;
-        for (let j = i + 1; j < s.length; j++){
-            if (check.has(s[j])) {
-                if (count > max) max = count;
-                break;
-            } else {
-                check.add(s[j]); 
-                count++;
-            }
-        }
-        if (count > max) max = count; 
+    for (let i = s.length - 1; i >= 0; i--) {
+       
     }
-   
-    return max;
-
 }
-
 console.log(lengthOfLongestSubstring("abcabcbb"))
--- 409-LongestPalindrome.js
+++ 409-LongestPalindrome.js
@@ -1,5 +1,6 @@
 function longestPalindrome(str) {
     const pairStorage = {};
+    let check = 0;
     for (let char of str) {
         if (char in pairStorage) {
             pairStorage[char]++;
@@ -11,8 +12,17 @@ function longestPalindrome(str) {
     for (let key in pairStorage) {
         if (pairStorage[key] % 2 === 0) {
             count += pairStorage[key];
+        } else if (pairStorage[key] > 1 && pairStorage[key] % 2 === 1) {
+            count += pairStorage[key] - 1;
+            check++;
+        } else if (pairStorage[key] === 1) {
+            check++;
         }
     }
+    if (check > 0) {
+        count++;
+    }
+
     return count;
 }
 
